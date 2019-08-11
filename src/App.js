25d749c2018dbf2eebeebb76f5e1008f2e5d4cfd
Zoran Ilievski <zoran@frankbody.com>
2019-08-11 14:14:35 +1000

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return <div>
+  <h1>React App</h1>
+    </div >
+  }
+  
+};
+
+export default App;
